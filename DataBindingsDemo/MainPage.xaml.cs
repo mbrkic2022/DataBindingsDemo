bebb3db22b5d6db8a68829b76a0e1bdca56206fe
Marija Brkić Bakarić <mbrkic@uniri.hr>
2025-03-20 11:22:30 +0100

    Add project files.

---
+++ DataBindingsDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace DataBindingsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void GreetMeBtn_Clicked(object sender, EventArgs e)
+        {
+            string greeting = "Hello, " + NameEntry.Text;
+            GreetingLbl.Text = greeting;
+        }
+    }
+
+}
